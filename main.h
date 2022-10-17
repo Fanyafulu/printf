19b50186280cf373f9522a967fc9c0d7a22526ef
Fanyafulu <fanyafulu@yahoo.com>
2022-10-17 06:07:53 -0700

    printf

--- main.h
+++ main.h
@@ -1,14 +1,13 @@
-#ifndef MAIN_H                               
+#ifndef MAIN_H                          
 #define MAIN_H
 #include <stdarg.h>
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)                                             
 #define BUFF_SIZE 1024
 
 /* FLAGS */
-
 #define F_MINUS 1                                                       
 #define F_PLUS 2                                                        
 #define F_ZERO 4                                                        
@@ -22,15 +21,14 @@
 /**
 * struct fmt - Struct op                                                
 * @fmt: The format.                                                     
-* @fn: The function associated.                                         
+* @fn: The function associated.
 */
 
 struct fmt
 {
 char fmt;
 int (*fn)(va_list, char[], int, int, int, int);
-}
-;
+};
 
 /**
 * typedef struct fmt fmt_t - Struct op
