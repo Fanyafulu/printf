526eb83f00577a21e7e7e9785fbb0cdd9cd6c9b0
Fanyafulu <fanyafulu@yahoo.com>
2022-10-17 04:38:52 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -14,9 +14,12 @@ int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
+
 if (format == NULL)
 return (-1);
+
 va_start(list, format);
+
 for (i = 0; format && format[i] != '\0'; i++)
 {
 if (format[i] != '%')
@@ -24,10 +27,10 @@ if (format[i] != '%')
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
-
 /* write(1, &format[i], 1);*/
 printed_chars++;
 }
+
 else
 {
 print_buffer(buffer, &buff_ind);
@@ -38,6 +41,7 @@ size = get_size(format, &i);
 ++i;
 printed = handle_print(format, &i, list, buffer,
 flags, width, precision, size);
+
 if (printed == -1)
 return (-1);
 printed_chars += printed;
@@ -52,7 +56,6 @@ return (printed_chars);
 * print_buffer - Prints the contents of the buffer if it exist
 * @buffer: Array of chars
 * @buff_ind: Index at which to add next char, represents the length.
-*
 */
 
 void print_buffer(char buffer[], int *buff_ind)
