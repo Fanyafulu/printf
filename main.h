f706ceda8ff777d972c3fc1d7a11f5f63a6ba0ea
Fanyafulu <fanyafulu@yahoo.com>
2022-10-18 15:23:53 -0700

    printf

--- main.h
+++ main.h
@@ -37,10 +37,11 @@ int (*fn)(va_list, char[], int, int, int, int);
 * @fmt: The format.
 * @fm_t: The function associated.
 */
+
 typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
 /****************** FUNCTIONS ******************/
@@ -94,6 +95,7 @@ char buffer[], int flags, int width, int precision, int size);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
+
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
