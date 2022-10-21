13a303c63cd5601de5a88bb1a3bdf111849653cb
Fanyafulu <fanyafulu@yahoo.com>
2022-10-21 10:56:13 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -10,44 +10,33 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+int i = 0, length = 0, j = 0;
+length = string_length(format, j);
+va_list args;
+va_start(args, format);
 
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
+while (i < length)
+
 {
 if (format[i] != '%')
 {
-buffer[buff_ind++] = format[i];
-
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
+write_char(format[i]);
+} else
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+i++;
+switch(format[i])
+{
+case 'd':
+{
+int x = va_arg(args, int);
+write_char(x + '0');
 }
 }
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
+}
+i++;
+}
+va_end(args);
+return (0);
 }
 
 /**
